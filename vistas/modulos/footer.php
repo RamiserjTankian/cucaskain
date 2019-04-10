3b827bf9e50020c64b6dc4445e084235a418c952
Estebanyzq <estebanyrazoqui@gmail.com>
2019-04-10 12:06:07 -0500

    Eliminacion de parte del Footer
    Quite la barra naranja para que se vea mejor el footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,67 +1,12 @@
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4 col-md-4 col-lg-4">
-                    <div class="widget clearfix">
-                    <div class="shadow p-3 mb-5 bg-white rounded">
-                        <div class="image2">
-                        <i class="fas fa-bug fa-7x"></i>
-                        </div>
-                        <div class="posicion2">
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dolorem fuga incidunt odit molestiae fugit ipsum sint architecto temporibus illo doloribus, quasi nihil sunt impedit amet nemo ex facilis itaque!"</p>
-                        </div>                        
-                    </div><!-- end clearfix -->
-                </div><!-- end col -->
-                </div><!-- end col -->
+<div class="copyrights">
+    <div class="container">
+        <div class="footer-distributed">
+            <div class="footer-left">                   
+                <p class="footer-company-name">Todos los derechos reservados &copy; 2019 <a href="inicio">Cucas Kain</a> Diseñado por: 
+				<a href="#">Haztech Consulting</a></p>
+            </div>            
+        </div><!-- end footer-distributed -->
+    </div><!-- end container -->
+</div><!-- end copyrights -->
 
-				<div class="col-sm-4 col-md-4 col-lg-4">
-                <div class="shadow p-3 mb-5 bg-white rounded">
-                <div class="container">
-                <div class="mapouter"><div class="gmap_canvas"><iframe width="250" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Werbung: <a href="https://www.pureblack.de">pureblack.de</a></div><style>.mapouter{position:relative;text-align:right;height:250px;width:250px;}.gmap_canvas {overflow:hidden;background:none!important;height:250px;width:250px;}</style></div>
-                    </div>
-                    </div><!-- end clearfix -->
-                </div><!-- end col -->
-
-				
-				<div class="col-sm-4 col-md-4 col-lg-4">
-                <div class="shadow p-3 mb-5 bg-white rounded">
-					<div class="widget footer-contact clearfix">
-						<div class="widget-title">
-                            
-                            <h3>Contactanos</h3>
-                        </div>
-    
-                            <ul>
-                                <li>Dirección: </li>
-                                <li>Télefono: <a href="#">+999999999</a></li>
-                                <li>Correo: <a href="#">cucaskain@lorem.lorem</a></li>
-                            </ul>
-                            <ul class="social-list">
-                                <li><a href="https://www.facebook.com/" class="facebook"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="https://twitter.com/" class="twitter"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="https://www.instagram.com/" class="instagram"><i class="fa fa-instagram"></i></a></li>
-                            </ul>
-						</div>
-					</div>
-				</div>
-				
-				
-				
-            </div><!-- end row -->
-     
-    </footer><!-- end footer -->
-
-    <div class="copyrights">
-        <div class="container">
-            <div class="footer-distributed">
-                <div class="footer-left">                   
-                    <p class="footer-company-name">Todos los derechos reservados &copy; 2019 <a href="inicio">Cucas Kain</a> Diseñado por: 
-					<a href="#">Haztech Consulting</a></p>
-                </div>
-
-                
-            </div>
-        </div><!-- end container -->
-    </div><!-- end copyrights -->
-
-    <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+<a href="inicio" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
