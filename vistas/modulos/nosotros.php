fd201b2a91fdfa888552b5ef3f50baf4b2856735
Estebanyzq <estebanyrazoqui@gmail.com>
2019-04-10 21:56:55 -0500

    Estructuras de vistas

--- vistas/modulos/nosotros.php
+++ vistas/modulos/nosotros.php
@@ -5,10 +5,17 @@
                 <p class="lead"> <br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, minima. A, est aliquam. Dolor fuga laboriosam quam debitis, voluptas adipisci ex impedit facere modi asperiores quisquam commodi, fugit, numquam quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam assumenda qui optio quo impedit sed ducimus, tempore ipsum, labore voluptatum, numquam esse placeat consectetur aperiam ipsa sapiente in corrupti modi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cum nostrum illo fugit maiores iusto consectetur, facere nam reiciendis omnis? Nesciunt consequatur illo, distinctio suscipit nisi iste sunt voluptatum error?</p>
             </div><!-- end title -->
            
-            <div id="about" class="section wb">
+<div id="about" class="section wb">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
+                    <div class="message-box">
+                        <h2>Lorem</h2>
+                        <p class="lead">Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus. Sed a tellus quis mi rhoncus dignissim.</p>
+                        <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vitae rutrum neque. Ut id erat sit amet libero bibendum aliquam. Donec ac egestas libero, eu bibendum risus. Phasellus et congue justo. </p>
+                        <button type="button" class="btn btn-warning btn-lg">Agendar</button>
+                    </div><!-- end messagebox -->
+                        <br><br>                        
                     <div class="message-box">
                         <h2>Lorem</h2>
                         <p class="lead">Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus. Sed a tellus quis mi rhoncus dignissim.</p>
@@ -16,93 +23,9 @@
                         <button type="button" class="btn btn-warning btn-lg">Agendar</button>
                     </div><!-- end messagebox -->
                 </div><!-- end col -->
-
-                <div class="col-md-6">
-                    <div class="video-container">
-                 
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rvqbX78QTWM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div><!-- end media -->
-              
-                </div><!-- end col -->
             </div><!-- end row -->        
         </div><!-- end container -->
-    </div><!-- end section -->
-            <div class="row">
-                <div class="col-md-12 col-sm-12">
-                    <div class="testi-carousel owl-carousel owl-theme">
-                        <div class="testimonial clearfix">
-                            <div class="desc">
-                                <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
-                                <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
-                            </div>
-                            <div class="testi-meta">
-                                <h4>James Fernando <small>- Manager of Racer</small></h4>
-                            </div>
-                            <!-- end testi-meta -->
-                        </div>
-                        <!-- end testimonial -->
-
-                        <div class="testimonial clearfix">
-                            <div class="desc">
-                                <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
-                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
-                            </div>
-                            <div class="testi-meta">                                
-                                <h4>Jacques Philips <small>- Designer</small></h4>
-                            </div>
-                            <!-- end testi-meta -->
-                        </div>
-                        <!-- end testimonial -->
-
-                        <div class="testimonial clearfix">
-                            <div class="desc">
-                                <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
-                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
-                            </div>
-                            <div class="testi-meta">
-                                <h4>Venanda Mercy <small>- Newyork City</small></h4>
-                            </div>
-                            <!-- end testi-meta -->
-                        </div>
-                        <!-- end testimonial -->
-                        <div class="testimonial clearfix">
-                            <div class="desc">
-                                <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
-                                <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
-                            </div>
-                            <div class="testi-meta">
-                                <h4>James Fernando <small>- Manager of Racer</small></h4>
-                            </div>
-                            <!-- end testi-meta -->
-                        </div>
-                        <!-- end testimonial -->
-
-                        <div class="testimonial clearfix">
-                            <div class="desc">
-                                <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
-                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
-                            </div>
-                            <div class="testi-meta">
-                                <h4>Jacques Philips <small>- Designer</small></h4>
-                            </div>
-                            <!-- end testi-meta -->
-                        </div>
-                        <!-- end testimonial -->
-
-                        <div class="testimonial clearfix">
-                            <div class="desc">
-                                <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
-                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
-                            </div>
-                            <div class="testi-meta">
-                                <h4>Venanda Mercy <small>- Newyork City</small></h4>
-                            </div>
-                            <!-- end testi-meta -->
-                        </div><!-- end testimonial -->
-                    </div><!-- end carousel -->
-                </div><!-- end col -->
-            </div><!-- end row -->
-            </div>
-            </div>
-
+    </div><!-- end section -->  
+</div>
+</div>
 <span class="ir-arriba"><i class="fas fa-angle-up"></i></span> 
--- vistas/modulos/plagas.php
+++ vistas/modulos/plagas.php
@@ -1,1 +1,51 @@
+<div class="banner-area banner-bg-1">
+<div class="container">
+<div class="shadow p-3 mb-5 bg-white rounded">
+<div class="row">
+    <div class="col-sm-6 col-md-6">
+        <img class="img-fluid" src="vistas/images/logo-cucas-kain.png" >
+         </div>
+            <div class="col-sm-6 col-md-6">
+                <div class="text-white text-justify">
+                    <h1>Lorem</h1>
+                    <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus. Sed a tellus quis mi rhoncus dignissim.</p>           
+                    <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vitae rutrum neque. Ut id erat sit amet libero bibendum aliquam. Donec ac egestas libero, eu bibendum risus. Phasellus et congue justo. </p>
+                    <button type="button" class="btn btn-warning btn-lg">Agendar</button>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+<div id="test-box" class="section wb">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>¿Qué son las plagas?</h3>
+                <p class="lead"> <br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, minima. A, est aliquam. Dolor fuga laboriosam quam debitis, voluptas adipisci ex impedit facere modi asperiores quisquam commodi, fugit, numquam quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam assumenda qui optio quo impedit sed ducimus, tempore ipsum, labore voluptatum, numquam esse placeat consectetur aperiam ipsa sapiente in corrupti modi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cum nostrum illo fugit maiores iusto consectetur, facere nam reiciendis omnis? Nesciunt consequatur illo, distinctio suscipit nisi iste sunt voluptatum error?</p>
+            </div><!-- end title -->
+           
+            <div id="about" class="section wb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="message-box">
+                        <h2>Lorem</h2>
+                        <p class="lead">Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus. Sed a tellus quis mi rhoncus dignissim.</p>
+                        <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vitae rutrum neque. Ut id erat sit amet libero bibendum aliquam. Donec ac egestas libero, eu bibendum risus. Phasellus et congue justo. </p>
+                    </div><!-- end messagebox -->
+                        <br><br>                        
+                    <div class="message-box">
+                        <h2>Lorem</h2>
+                        <p class="lead">Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus. Sed a tellus quis mi rhoncus dignissim.</p>
+                        <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vitae rutrum neque. Ut id erat sit amet libero bibendum aliquam. Donec ac egestas libero, eu bibendum risus. Phasellus et congue justo. </p>
+                    </div><!-- end messagebox -->
+                </div><!-- end col -->
+            </div><!-- end row -->        
+        </div><!-- end container -->
+    </div><!-- end section -->
+            
+</div>
+</div>
 <span class="ir-arriba"><i class="fas fa-angle-up"></i></span>   
